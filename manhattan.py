42d1b0d4889ac4e8d2ceb0c60a0c039d9b3e2e0f
GiannosL <giannos.louloudis@gmail.com>
2023-06-06 14:31:33 +0200

    titles and labels

--- manhattan.py
+++ manhattan.py
@@ -4,7 +4,7 @@ from source.manhattan_plot import man_plot
 from source.preprocessing import pre_processing
 
 
-def manhattan(df):
+def manhattan(df, plot_title='Manhattan plot'):
     """
     doc
     """
@@ -19,7 +19,12 @@ def manhattan(df):
              plot_ax=ax,
              y_column='-log(p)')
     
-    # 
+    # titles, labels
+    plt.title(plot_title, fontsize=20, weight='bold')
+    plt.xlabel('Chromosome', fontsize=16)
+    plt.ylabel('Negative log10(p-value)', fontsize=16)
+
+    #
     max_neglog = df['-log(p)'].max() if df['-log(p)'].max() > 9 else 9
     plt.ylim((0, max_neglog+2))
     plt.show()
