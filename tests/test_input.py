576367a10eba5918000369ca6dd9d6b51c79ed4d
GiannosL <giannos.louloudis@gmail.com>
2023-06-12 15:21:20 +0100

    first tests

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,3 +1,15 @@
-class TestManhattan:
-    def test_input(self):
-        assert(1 == 1)
+from source.view_models.gwas_vm import parse_snp_file, parse_gwas_file
+
+class TestGuiFuncs:
+    def test_parse_snp_file(self):
+        filename: str = 'tests/snps_list.txt'
+        snp_list: list[str] = parse_snp_file(snp_filename=filename)
+
+        assert(snp_list.__len__() == 5)
+
+    def test_parse_gwas_file(self):
+        filename: str = 'tests/gwas_file.tsv'
+        df = parse_gwas_file(gwas_filename=filename)
+        print(df)
+
+        assert(df.shape == (5,6))
